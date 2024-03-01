1c040a9fef0d8d1836cefd5ad614f4aef19ff02f
jakem <mazurkiewiczj@duq.edu>
2024-03-01 08:11:45 -0500

    created columns of interest

--- COLLEGEIMPROVEMENT/data_reader.py
+++ COLLEGEIMPROVEMENT/data_reader.py
@@ -13,12 +13,23 @@ def data_reader():
         '2015_16', '2016_17', '2017_18', '2018_19', '2019_20', '2020_21', '2021_22'
     ]
 
+    columns_of_interest = ['HIGHDEG', 'CCSIZSET', 'ADM_RATE', 'SATVR75', 'SATMT75', 'PPTUG_EF',
+                           'NPT44_PRIV', 'TUITIONFEE_IN', 'AVGFACSAL', 'PFTFAC', 'INEXPFTE',
+                           'ENRL_ORIG_YR2_RT', 'FIRSTGEN_WDRAW_ORIG_YR3_RT', 'COMPL_RPY_7YR_RT',
+                           'GRAD_DEBT_MDN', 'MDEARN_ALL', 'CIPTFBS1', 'MTHCMP1', 'BBRR2_FED_UG_NOPROG',
+                           'BOOKSUPPLY', 'ROOMBOARD_ON', 'OTHEREXPENSE_ON', 'ROOMBOARD_OFF', 'OTHEREXPENSE_OFF',
+                           'OTHEREXPENSE_FAM', 'STUFACR', 'IRPS_NRA', 'COUNT_NWNE_4YR'
+    ]   
+
     df = pd.read_csv(r'C:\Users\jakem\CollegeImprovement\COLLEGEIMPROVEMENT\data\data\MERGED1997_98_PP.csv')
 
     dataframes = {}  
 
     for year in years_list:
         df = pd.read_csv(fr'C:\Users\jakem\CollegeImprovement\COLLEGEIMPROVEMENT\data\data\MERGED{year}_PP.csv')
+
+        df = df[columns_of_interest]
+
         #Filtering out dataframes for Duquesne
         df = df.loc[df['INSTNM'] == 'Duquesne University']
 
